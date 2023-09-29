1211b54babec34fb32202ca2dc9f351c8c14dae3
Kartik Kankurte <kartikkankurte@gmail.com>
2023-09-29 16:39:06 +0530

    made cart page responsive

--- frontend/src/pages/Cart.js
+++ frontend/src/pages/Cart.js
@@ -1,11 +1,13 @@
 import React from 'react'
-import { useSelector } from 'react-redux'
 import '../styles/cart.css'
 import SideBar from '../components/SideBar'
 import emtycart from '../assests/emtycart.gif'
 import CartItemCard from '../components/CartItemCard'
 import { Link } from 'react-router-dom'
 import { useNavigate } from 'react-router-dom'
+import { HiMenuAlt1 } from 'react-icons/hi'
+import {useDispatch, useSelector} from 'react-redux'
+import { searchProducts, showSideBar } from '../actions'
 const Cart = () => {
     const cartItems = useSelector(state=>state.cart.cartItems)
     const cartPrice=cartItems.reduce((total,itm)=>total+itm?.price*itm?.qty,0)
@@ -14,6 +16,8 @@ const Cart = () => {
     const totalPrice= (cartPrice+deleviryPrice)-discount;
     const auth = useSelector(state=>state.user.user)
     const navigate = useNavigate()
+    const dispatch =useDispatch()
+
     const checkoutHandler =()=>{
         if(auth){
             navigate(`/shipping`)
@@ -24,7 +28,10 @@ const Cart = () => {
     }
     return (
         <>
-        <SideBar/>
+        <div className="burger" onClick={()=>dispatch(showSideBar(true))}>
+               <HiMenuAlt1/>
+        </div>
+        <SideBar className='sidebar'/>
         <div className='cart-screen'>
             <h1>MY CART</h1>
             {
